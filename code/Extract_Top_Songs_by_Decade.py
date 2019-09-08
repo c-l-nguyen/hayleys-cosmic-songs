1f96d5ec39c314180c3d5f719386bdcec666ec9e
c-l-nguyen <christopher811@gmail.com>
2019-09-07 20:33:32 -0500

    moved data again and extracted broad_genre

--- code/Extract_Top_Songs_by_Decade.py
+++ code/Extract_Top_Songs_by_Decade.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 
-decade_df = pd.read_csv("../data/billboard_hot_100_by_decade.csv")
+decade_df = pd.read_csv("../data/raw/billboard_hot_100_by_decade.csv")
 decade_df["decade"] = decade_df["decade"].astype("int64")
 
 # sort and keep top 100 songs for each decade
@@ -11,4 +11,4 @@ sorted_decade = decade_df.sort_values(["decade","count"], ascending=False).reset
 top_100_songs_by_decade = sorted_decade.groupby("decade").head(100)
 
 # save to CSV
-top_100_songs_by_decade.to_csv("../data/billboard_top_100_by_decade.csv", index=False)
+top_100_songs_by_decade.to_csv("../data/clean/decades/billboard_top_100_by_decade.csv", index=False)
